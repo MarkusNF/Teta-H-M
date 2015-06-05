96053205ba95b2a2b15cd5a9abfd7e452d391a33
alizhj <lisahjarpe@gmail.com>
2015-06-05 14:24:53 +0200

    DragDropCloneBackground

--- index.php
+++ index.php
@@ -4,33 +4,33 @@
 	<title>Outfitmaker</title>
 	<meta charset="UTF-8" />
 	<link rel="stylesheet" href="style.css">
-
-	
-
-	
-
 </head>
 
 <body>
 	<h1>Outfitmaker</h1>
+
 	<div id="clothes">
-		<img class="pic" src="img/minion1.png">
-		<img class="pic" src="img/bowser.png">
-		<img class="pic" src="img/star.png">
-		<img class="pic" src="img/svamp.png">
-		<img class="pic" src="img/minion2.png">
-	</div>
+		<div id="drag1" class="pic"></div> 
+		<div id="drag2" class="pic"></div> 
+		<div id="drag3" class="pic"></div> 
+		<div id="drag4" class="pic"></div> 
+		<div id="drag5" class="pic"></div> 
+	</div> <!-- end of options -->
+
 	<div id="bed"></div>
 	<div id="bin"></div>
+
 	<div class="clearfix"></div>
+	
 	<select name="categories" id="background">
 		<option value="backgroundDefault">Choose background</option>
 		<option value="summer">Summer</option>
 		<option value="beach">Beach</option>
 	</select>
+
 	
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="main.js"></script>
 </body>
 </html>
--- main.js
+++ main.js
@@ -1,33 +1,66 @@
+var counter, pos, element;
+
 $(document).ready(function(){
- 	imgDrag();
- 	chooseBackground();
+    dragImg();
+    chooseBackground();
+
 });
- 
-function imgDrag() {
-	$('img').draggable( {
-		containment: '#bed',
-		cursor: 'move'
-		//snap: '#content'
-	});
-
-	 $('.bin').droppable({
-        drop: function(event, ui) {
-            ui.draggable.remove();
-        }
-    });
-
-	$('#bed').droppable({
-        drop: function(event, ui) {
-            ui.draggable;
-        }
-    });
-}
 
 function chooseBackground() {
-	$('#background').change(function() {
-		var backgroundImage = $('#background').val();
-		$('select option:selected').each(function() {
-			$('#bed').html('<img class="board" src="img/' + backgroundImage + '.jpg"/><div class="bin"></div>');
-		});
-	}).trigger('change');
+    $('#background').change(function() {
+        var backgroundImage = $('#background').val();
+        $('select option:selected').each(function() {
+            $('#bed').html('<img class="board" src="img/' + backgroundImage + '.jpg"/><div class="bin"></div>');
+        });
+    }).trigger('change');
+}
+
+function dragImg(){
+    //Counter
+    counter = 0;
+    //Make element draggable
+    $(".pic").draggable({
+        helper:'clone',
+        containment: '#bed',
+
+        //When first dragged
+        stop:function(ev, ui) {
+        	var pos=$(ui.helper).offset();
+        	objName = "#clonediv"+counter
+        	$(objName).css({"left":pos.left,"top":pos.top});
+        	$(objName).removeClass("pic");
+
+
+           	//When an existiung object is dragged
+            $(objName).draggable({
+            	containment: 'parent',
+                stop:function(ev, ui) {
+                	var pos=$(ui.helper).offset();
+                	console.log($(this).attr("id"));
+					console.log(pos.left)
+                    console.log(pos.top)
+                }
+            });
+        }
+    });
+    //Make element droppable
+    $("#bed").droppable({
+		drop: function(ev, ui) {
+			if (ui.draggable.attr('id').search(/drag[0-9]/) != -1){
+				counter++;
+				var element=$(ui.draggable).clone();
+				element.addClass("tempclass");
+				$(this).append(element);
+				$(".tempclass").attr("id","clonediv"+counter);
+				$("#clonediv"+counter).removeClass("tempclass");
+
+				//Get the dynamically item id
+				draggedNumber = ui.draggable.attr('id').search(/drag([0-9])/)
+				itemDragged = "dragged" + RegExp.$1
+				console.log(itemDragged)
+
+				$("#clonediv"+counter).addClass(itemDragged);
+			}
+    	}
+    });
 }
