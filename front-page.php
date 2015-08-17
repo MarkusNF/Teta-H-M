6a177a0c1f7416fcec317f37fb015ce4ead4c2da
dcastroe <dilica84@hotmail.com>
2015-08-17 13:36:43 +0200

    Nytt

--- front-page.php
+++ front-page.php
@@ -14,37 +14,50 @@ get_header(); ?>
 
 <div class="wrapper">
 	<div id="banner">
-		<a href="http://www.hm.com/se/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/hm_logo2.png" alt="HM logga"></a>
+		<!-- <a href="http://www.hm.com/se/" target="_blank"><img src="<?php //echo get_template_directory_uri(); ?>/img/hm_logo2.png" alt="HM logga"></a> -->
 		<h1><?php bloginfo('name') ?></h1>
 	</div><!-- #banner -->
 
+	<div id="loves">
+		
+	</div>
+
+	<div id="info">
+		<p>Beställ din outfit här, hämta den på din slutdestination</p>
+	</div>
+
 	<div class="gender female"></div>
 	<div class="gender male"></div>
+	<div class="gender hen"></div>
 	<div class="category">
 
-	<?php
-	$taxonomies = array( 
-		'gender_category',
-	);
+		<?php
+		$taxonomies = array( 
+			'gender_category',
+		);
 
-	$args = array(
-		'orderby'			=> 'id', 
-		'order'				=> 'ASC',
-		'hide_empty'		=> false,
-		'include'			=> array(6, 7)
-		//'include'			=> array(2, 3)
-	); 
+		$args = array(
+			'orderby'			=> 'id', 
+			'order'				=> 'ASC',
+			'hide_empty'		=> false,
+			//'include'			=> array(6, 7, 17)
+			'include'			=> array(2, 3, 17)
+		); 
 
-	$terms = get_terms($taxonomies, $args);
+		$terms = get_terms($taxonomies, $args);
 
-	foreach( $terms as $term ) {
+		?>
+		<ul>
+			<?php
+			foreach( $terms as $term ) {
 
-		// Get the term link
-		$term_link = get_term_link( $term );
+				// Get the term link
+				$term_link = get_term_link( $term );
 
-		echo '<ul><li><a href="' . esc_url( $term_link ) . '">' . $term->name .'</a></li></ul>';
-	}
-	?>
+				echo '<li><a href="' . esc_url( $term_link ) . '">' . $term->name .'</a></li>';
+			}
+			?>
+		</ul>
 	</div><!-- .category -->
 </div><!-- .wrapper -->
 
